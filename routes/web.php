d00f9744175125c61a068cffa005052acdc12da1
Kuroyukihime00 <abhivandyaaudryriana@gmail.com>
2025-03-05 07:49:27 +0700

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/1', function () {
+    return view('demo/file1');
+});
+Route::get('/2', function () {
+    return view('demo/file2');
+});
+use App\Http\Controllers\DemoController;
+
+Route::get('/3', [DemoController::class, 'index']);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
